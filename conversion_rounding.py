437ee24479d10e339fe75c17982d8dd77b96a33a
Ronan O'Brien-Hall <135774404+DarkenedBlood@users.noreply.github.com>
2026-02-11 13:34:57 +1300

    Working Conversion Component

---
+++ conversion_rounding.py
@@ -0,0 +1,29 @@
+def round_ans(val):
+    """
+    Rounds temperatures to nearest degree
+    :param val: Number to be rounded
+    :return: Number Rounded to nearest degree
+    """
+    var_rounded = (val * 2 + 1) // 2
+    return "{:.0f}".format(var_rounded)
+
+
+def to_celsius(to_convert):
+    """
+    Converts from °F to °C
+    :param to_convert: Temperature to be converted in °F
+    :return:  Converted temperature in °C
+    """
+    answer = (to_convert - 32) * 5 / 9
+    return round_ans(answer)
+
+
+def to_fahrenheit(to_convert):
+    """
+    Converts from °F to °C
+    :param to_convert: Temperature to be converted in °C
+    :return: Converted temperature in °F
+    """
+    answer = to_convert * 1.8 + 32
+    return round_ans(answer)
+
